9910aad86c56cc6f10f9e576fc1fb7c445812579
digital-BHQ <digital@bangkokhospital.com>
2025-10-12 21:32:54 +0700

    Update boot.js

--- boot.js
+++ boot.js
@@ -2,7 +2,7 @@ function boot() {
   try { initArrowCarousel('#carousel', '#cards'); } catch(e){ console.error(e); }
   try { initTheatreCarousel(); } catch(e){ console.error(e); }
 }
-
+ 
 if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', boot);
 } else {
